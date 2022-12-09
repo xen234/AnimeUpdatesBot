3cfb934f33fb51ed4c105a120950d94b607109ca
chmousNedovolniy <89921408+chmousNedovolniy@users.noreply.github.com>
2022-12-09 14:42:17 +0300

    Add files via upload

--- Bot/bot_config.py
+++ Bot/bot_config.py
@@ -1,11 +1,17 @@
 import logging
+from os import environ
+
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from config import TOKEN
 
+from database import Database
+from api import JikanWrapper
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=environ['TOKEN'])
 dp = Dispatcher(bot)
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('broadcast')
+
+database = Database()
+api = JikanWrapper()
--- Bot/bot_main.py
+++ Bot/bot_main.py
@@ -1,21 +1,23 @@
-from send_message_function import send_message
 import re
-from bot_config import dp
+
 from aiogram import types
 from aiogram.utils import executor
+
+from bot_config import dp, database, api
+from send_message_function import send_message
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Для просмотра доступных команд отправь /help ")
+    await message.reply("Привет! Для просмотра доступных команд отправь /help ")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("В этом боте ты можешь выполнить следующие команды:\n"
-                        "/list - посмотреть список отслеживаемых аниме\n"
                         "/subscribe URL RANGE - оформить подписку на обновления аниме по ссылке\n"
                         "/unsubscribe URL RANGE - оформить подписку на обновления аниме по ссылке\n"
+                        "/list - посмотреть список отслеживаемых аниме\n"
                         "/weekly - посмотреть расписание выхода эпизодов отслеживаемых аниме на неделе\n"
                         "В командах subscribe и unsubscribe необходимо передать на вход ссылку "
                         "на интересующее аниме с ресурса MyAnimeList в следующем формате:\n"
@@ -24,9 +26,28 @@ async def process_help_command(message: types.Message):
 
 
 @dp.message_handler(commands=['list'])
-async def process_info_command(message: types.Message):
-    await message.reply(f"Всего отслеживаемых аниме: "
-                        f"\n")
+async def process_list_command(message: types.Message):
+    anime_list = database.get_tg_subscriptions_by_chat(message.from_user.id)
+    await message.reply("Всего отслеживаемых аниме: {}\n".format(len(anime_list)))
+    for anime_id in anime_list:
+        await send_message(message.from_user.id, api.get_url_by_id(anime_id))
+
+
+@dp.message_handler(commands=['weekly'])
+async def process_weekly_command(message: types.Message):
+    anime_list = database.get_tg_subscriptions_by_chat(message.from_user.id)
+    for weekday in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']:
+        weekday_anime_titles = list()
+        anime_on_weekday = api.scheduled_on_week_day(weekday)
+
+        for anime in anime_on_weekday:
+            if anime.id in anime_list:
+                weekday_anime_titles.append((anime.title, api.get_url_by_id(int(anime.id))))
+
+        if len(weekday_anime_titles):
+            await send_message(message.from_user.id, weekday.upper())
+            for title, link in weekday_anime_titles:
+                await send_message(message.from_user.id, title + '\t' + link)
 
 
 # ----------------подписка на анимы---------------------------
@@ -37,16 +58,17 @@ async def process_subscribe_command(message: types.Message):
                         f" воспользовавшись командой /help")
     input_url = re.split(' ', message.text, maxsplit=3)
 
-    try:
-        input_url = int(input_url[1])
-        if 'myanimelist' not in input_url:
-            raise ValueError
-    except ValueError:
-        return await message.reply(f"Вы неверно ввели ссылку. Попробуйте еще раз :)")
+    # try:
+    #     input_url = input_url[1]
+    #     if 'myanimelist' not in input_url:
+    #         raise ValueError
+    # except ValueError:
+    #     return await message.reply(f"Вы неверно ввели ссылку. Попробуйте еще раз :)")
 
     # check if suitable
-    database.create_subscription(message.from_user.id, input_url)
-    await message.reply(f"Вы успешно подписались на обновления по следующей ссылке: {input_url}")
+    anime_id = int(api.parse_url(input_url[1])[1])
+    database.create_subscription(message.from_user.id, anime_id)
+    await message.reply(f"Вы успешно подписались на обновления по следующей ссылке: {input_url[1]}")
 
 
 @dp.message_handler(commands=['unsubscribe'])
@@ -54,28 +76,18 @@ async def process_cancel_subscription_command(message: types.Message):
     await message.reply(f"На вход необходимо подать ссылку. Подробности можно узнать,"
                         f" воспользовавшись командой /help")
     input_url = re.split(' ', message.text, maxsplit=3)
-    try:
-        input_url = int(input_url[1])
-        if 'myanimelist' not in input_url:
-            raise ValueError
-    except ValueError:
-        return await message.reply(f"Вы неверно ввели ссылку. Попробуйте еще раз :)")
+    # try:
+    #     input_url = int(input_url[1])
+    #     if 'myanimelist' not in input_url:
+    #         raise ValueError
+    # except ValueError:
+    #     return await message.reply(f"Вы неверно ввели ссылку. Попробуйте еще раз :)")
 
     # check if suitable
-    database.remove_tg_subscription(message.from_user.id, input_url)
-    await message.reply(f"Вы успешно отписались от обновлений по следующей ссылке: {input_url}")
-
-
-@dp.message_handler(lambda message: len(database.get_tg_subscriptions_by_chat(message.chat.id)))
-async def process_forward_command(message: types.Message):
-    subscriptions = database.get_tg_subscriptions_by_chat(message.chat.id)
-    print(subscriptions)
-    if message_classifier.is_important(message.text):
-        for user in subscriptions:
-            await send_message(user.user_id, message.text)
+    anime_id = int(api.parse_url(input_url[1])[1])
+    database.remove_tg_subscription(message.from_user.id, anime_id)
+    await message.reply(f"Вы успешно отписались от обновлений по следующей ссылке: {input_url[1]}")
 
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-
-    
