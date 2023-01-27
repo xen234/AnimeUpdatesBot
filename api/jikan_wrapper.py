409a654f91cce4b2c71c32d359b61379c5a15e28
chmousNedovolniy <89921408+chmousNedovolniy@users.noreply.github.com>
2023-01-27 18:16:11 +0300

    Update jikan_wrapper.py

--- api/jikan_wrapper.py
+++ api/jikan_wrapper.py
@@ -1,22 +1,9 @@
 import re
 import time
 from typing import List, Tuple, Union
+from api.models import AnimeEpisode, Anime
 
 import requests
-
-class Anime:
-    def __init__(self, _id: str, url: str, title: str, broadcast: str):
-        self.id = _id
-        self.url = url
-        self.title = title
-        self.broadcast = broadcast
-
-
-class AnimeEpisode:
-    def __init__(self, url: str, title: str, aired: str):
-        self.url = url
-        self.title = title
-        self.aired = time.strptime(aired, "%Y-%m-%dT%H:%M:%S+00:00")
 
 
 class JikanWrapper:
@@ -35,12 +22,12 @@ class JikanWrapper:
     @staticmethod
     def _make_error_text(status_code) -> str:
         if status_code == 404:
-            return "page not found"
+            return "такая страница не найдена"
         if status_code >= 500:
-            return "server error"
+            return "на сервере произошла ошибка"
         if status_code == 429:
-            return "too many requests"
-        return "unknown error"
+            return "слишком много запросов"
+        return "ошибка неизвестна"
 
     def get_url_by_id(self, anime_id: int) -> str:
         return self.base_anime_url + str(anime_id) + '/'
@@ -59,7 +46,7 @@ class JikanWrapper:
                 return False, self._make_error_text(response.status_code)
             data = response.json()
         last_episode = data["data"][-1]
-        return True, AnimeEpisode(last_episode["url"], last_episode["title"], last_episode["aired"])
+        return True, AnimeEpisode(last_episode["url"], last_episode["title"], last_episode["aired"], 0)
 
     def scheduled_on_week(self) -> Tuple[bool, Union[List[Anime], str]]:
         page = 1
@@ -103,15 +90,28 @@ class JikanWrapper:
 
     def parse_url(self, url: str) -> Tuple[bool, str]:
         if not url.startswith(self.base_anime_url):
-            return False, "wrong prefix"
+            return False, "некорретный сайт. Проверьте, что используете ресурс MyAnimeList https://myanimelist.net/anime/"
         response = requests.get(url)
         if response.status_code == 404:
-            return False, "invalid url"
+            return False, "некорретный url. Проверьте, что такое аниме действительно существует"
         _id = re.findall(r'\d+', url[len(self.base_url):])[0]
         url = self.base_url + f"anime/{_id}"
         response = JikanWrapper._make_request(url)
         if not response.ok:
-            return False, "invalid id"
+            return False, "некорретный id. Проверьте, что такое аниме действительно существует"
         if response.json()["data"]["type"] != "TV":
-            return False, "not a broadcasting anime"
+            return False, "Это не эпизодическое аниме, выберите эпизодическое аниме"
         return True, _id
+
+    def get_anime_info(self, anime_id: int) -> Anime:
+        url = self.base_url + str(anime_id)
+        response = JikanWrapper._make_request(url)
+        data = response.json()["data"]
+        return Anime(data["anime_id"],
+                     data["url"],
+                     data["title"],
+                     data["broadcast"]["string"],
+                     data["episodes"] if data["episodes"] is not None else "unknown"
+                     )
+
+
