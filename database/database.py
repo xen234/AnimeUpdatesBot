fee6230393c655e8cc0acb7a905cd34f1c8a1baa
chmousNedovolniy <89921408+chmousNedovolniy@users.noreply.github.com>
2023-01-27 18:14:51 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,4 +1,13 @@
 import json
+from typing import List
+
+import psycopg2
+import requests
+import time
+
+from api import Anime
+from database.models import User
+from database.queries import SqlQueries
 
 
 class Database:
@@ -53,7 +62,7 @@ class JsonLikeDatabase:
             self.add_user(user_id)
             user_list = self._read_from_db()
         if anime_url in user_list["users"][user_id]:
-            return False, "anime already added"
+            return False, "данное аниме уже добавлено"
         user_list["users"][user_id].append(anime_url)
         self._write_to_db(user_list)
         return True, None
@@ -66,7 +75,7 @@ class JsonLikeDatabase:
             self.add_user(user_id)
             user_list = self._read_from_db()
         if anime_url not in user_list["users"][user_id]:
-            return False, "anime not found"
+            return False, "такого аниме нет в базе данных"
         user_list["users"][user_id].remove(anime_url)
         self._write_to_db(user_list)
         return True, None
@@ -79,3 +88,151 @@ class JsonLikeDatabase:
         user_list = self._read_from_db()
         anime_list = user_list["users"][user_id]
         return True, anime_list
+
+
+class PostgreDB:
+    def __init__(self, recreate=False):
+        self.conn = psycopg2.connect("host=84.23.53.18 dbname=PostgreSQL-8678 user=sanchmous password=tB71pMP10T8oJ512")
+        if recreate:
+            with self.conn.cursor() as cur:
+                cur.execute(*SqlQueries.create_users_table)
+                cur.execute(*SqlQueries.create_anime_table)
+                cur.execute(*SqlQueries.create_link_table)
+                self.conn.commit()
+        else:
+            with self.conn.cursor() as cur:
+                cur.execute(*SqlQueries.create_users_table_if_not_exists)
+                cur.execute(*SqlQueries.create_anime_table_if_not_exists)
+                cur.execute(*SqlQueries.create_link_table_if_not_exists)
+                self.conn.commit()
+
+    def user_exists(self, _id) -> bool:
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.find_user_by_id, _id))
+            return cur.rowcount > 0
+
+    def anime_exists(self, _id) -> bool:
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.find_anime_by_id, _id))
+            return cur.rowcount > 0
+
+    def add_user(self, user: User):
+        if self.user_exists(user.id):
+            return
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.insert_user, str(user.id), user.name))
+            self.conn.commit()
+
+    def add_anime(self, anime: Anime):
+        if self.anime_exists(anime.id):
+            return
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.insert_anime,
+                                   str(anime.id), anime.url, anime.title, anime.broadcast, anime.aired_episodes))
+            self.conn.commit()
+
+    def subscribe(self, user_id: int, anime_id: int):
+        if not self.user_exists(user_id):
+            self.add_user(User(user_id, "undefined"))
+        if not self.anime_exists(anime_id):
+            self.add_anime(self.get_anime_info(anime_id))
+        if self.check_subscription(user_id, anime_id):
+            return False, "вы уже подписаны на это аниме! Не хотите попробовать что-то новое?"
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.add_reference, str(user_id), str(anime_id)))
+            self.conn.commit()
+        return True, None
+
+    def check_subscription(self, user_id: int, anime_id: int) -> bool:
+        if not self.user_exists(user_id):
+            return False
+        if not self.anime_exists(anime_id):
+            return False
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.user_anime_select, str(user_id), str(anime_id)))
+            return cur.rowcount > 0
+
+    def unsubscribe(self, user_id: int, anime_id: int):
+        if not self.user_exists(user_id):
+            self.add_user(User(user_id, "undefined"))
+        if not self.anime_exists(anime_id):
+            self.add_anime(self.get_anime_info(anime_id))
+        if not self.check_subscription(user_id, anime_id):
+            return False, "вы не подписаны на это аниме! Выберите аниме, на которое вы подписаны?"
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.remove_reference, str(user_id), str(anime_id)))
+            self.conn.commit()
+        return True, None
+
+    def users_subscriptions(self, user_id: int) -> (bool, List[Anime]):
+        if not self.user_exists(user_id):
+            self.add_user(User(user_id, "undefined"))
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.subscriptions, str(user_id)))
+            for i in range(cur.rowcount):
+                res.append(Anime(*cur.fetchone()))
+        return True, res
+
+    def anime_subscribers(self, anime_id: int) -> List[User]:
+        if not self.anime_exists(anime_id):
+            self.add_anime(self.get_anime_info(anime_id))
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.subscribers, str(anime_id)))
+            for i in range(cur.rowcount):
+                res.append(User(*cur.fetchone()))
+        return res
+
+    def get_users(self) -> List[User]:
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(*SqlQueries.select_all_users)
+            for i in range(cur.rowcount):
+                res.append(User(*cur.fetchone()))
+        return res
+
+    def get_all_anime(self) -> List[Anime]:
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(*SqlQueries.select_all_anime)
+            for i in range(cur.rowcount):
+                res.append(Anime(*cur.fetchone()))
+        return res
+
+    def get_anime_by_id(self, anime_id: int) -> Anime:
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.find_anime_by_id, str(anime_id)))
+            for i in range(cur.rowcount):
+                res.append(Anime(*cur.fetchone()))
+        return res[0]
+
+    def get_user_by_id(self, user_id: int) -> User:
+        res = []
+        with self.conn.cursor() as cur:
+            cur.execute(str.format(*SqlQueries.find_anime_by_id, str(user_id)))
+            for i in range(cur.rowcount):
+                res.append(User(*cur.fetchone()))
+        return res[0]
+
+    @staticmethod
+    def _make_request(url) -> requests.Response:
+        response = requests.get(url)
+        if response.status_code == 429:
+            time.sleep(1)
+            response = requests.get(url)
+        return response
+
+    def get_anime_info(self, anime_id: int) -> Anime:
+        url = "https://api.jikan.moe/v4/anime/" + str(anime_id)
+        response = self._make_request(url)
+        data = response.json()["data"]
+        return Anime(data["mal_id"],
+                     data["url"],
+                     data["title"],
+                     data["broadcast"]["string"],
+                     0)
+
+    def close(self):
+        self.conn.close()
